99ecc3a55d39f370c7169e69cff8bd42a6dbd925
Miguel Casqueira <miguel@balena.io>
2021-08-19 23:53:23 -0400

    Check if context is PR before finalizing
    Change-type: patch
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ This action allows you to send some source to Balena builders. Depending on the 
 jobs:
   balena_cloud_build:
     runs-on: ubuntu-latest
-    name: Build release for fleet
     steps:
       - uses: actions/checkout@v2
       - uses: balena-io/balena-ci@master
@@ -16,25 +15,28 @@ jobs:
         with:
           balena_token: ${{ secrets.BALENA_TOKEN }}
           fleet: sample-fleet
-          workflow_name: balena_cloud_build
       - name: Log release ID built
         run: echo "Built release ID ${{ steps.build.outputs.release_id }}"
 ```
 
 ## Inputs
 
-| key | Description | Required |
-| --- | --- | --- |
-| balena_token | API key to balenaCloud | true
-| fleet | Name of fleet to make releases for | true
-| workflow_name | name of your workflow running this action. This is necessary to attach some metadata to be looked up again when the action runs on merge to main. | true
-| versionbot | Tells action to use Versionbot branch for versioning | false
+| key | Description | Required | Default |
+| --- | --- | --- | --- |
+| balena_token | API key to balenaCloud | true | |
+| fleet | Name of fleet to make releases for | true | |
+| api_endpoint | Domain of API hosting your fleets | false | balena-cloud.com |
+| versionbot | Tells action to use Versionbot branch for versioning | false | false |
+
+The `fleet` input can be a single or  comma seperated list of fleets. This allows you to push source to several fleets which might be running different architectures. If your source code contains a Dockerfile.template the CLI will figure out how to build your release appropriately.
+
+Additionally, `api_endpoint` can be used to specify a custom domain for the backend that will build and deploy your release. If for example you want to deploy to balenaCloud staging environment, you would set it to `balena-staging.com`.
 
 ## Outputs
 
 | key | Description |
 | --- | --- |
-| release_id | ID of the release build
+| release_id | ID of the release built |
 
 ## Workflows
 
--- src/action.ts
+++ src/action.ts
@@ -7,7 +7,7 @@ import * as git from './git';
 
 export async function run(): Promise<void> {
 	// ID of the workflow this action is running in (to store and get data from)
-	const workflowName = core.getInput('workflow_name');
+	const workflowName = core.getInput('workflow_name', { required: true });
 
 	// Let's try to find if this workflow previously built a release
 	const previousReleaseId = await github.getFromWorkflow(
@@ -15,8 +15,13 @@ export async function run(): Promise<void> {
 		'releaseId',
 	);
 
-	// If the workflow contains a previous release then just mark it as final
-	if (previousReleaseId) {
+	// If the action is running in the context of a pull request then build with draft flag
+	// Otherwise, this release will be marked as final.
+	// This is for workflows where you push directly to branches without PR so drafts are not needed
+	const isDraft = github.isPullRequest();
+
+	// If the workflow contains a previous release and is not a pull request then just mark it as final
+	if (previousReleaseId && !isDraft) {
 		// This happens when the action ran during a PR and now it is merged so mark as final
 		await balena.finalize(previousReleaseId);
 		return;
@@ -25,25 +30,21 @@ export async function run(): Promise<void> {
 	// Now we know we're going to push some code so let's get some more variables...
 
 	// Name of fleet to build releases for
-	const fleet = core.getInput('fleet');
+	const fleet = core.getInput('fleet', { required: true });
 	// Indicate to action if repo uses Versionbot for vesrioning
 	const hasVersionbot = core.getInput('versionbot');
 	// File path to release source code
 	const src = process.env.GITHUB_WORKSPACE || '';
 	// Extract info about the git repository
-	const { name } = github.repositoryMetaContext();
+	const { name: repoName } = github.repositoryMetaContext();
 
 	// If the repository uses Versionbot then checkout Versionbot branch
 	if (hasVersionbot) {
 		const versionbotBranch = await versionbot.getBranch();
 		// This will checkout the branch to the `GITHUB_WORKSPACE` path
-		await git.checkout(name, versionbotBranch);
+		await git.checkout(repoName, versionbotBranch);
 	}
 
-	// If the action is running in the context of a pull request then build with draft flag
-	// Otherwise, this release will be marked as final.
-	// This is for workflows where you push directly to branches without PR so drafts are not needed
-	const isDraft = github.isPullRequest();
 	// Now send the source to the builders
 	const releaseId = await balena.push(fleet, src, isDraft);
 	// If this is a draft release let's persist the ID to the workflow so it can be finalized later
