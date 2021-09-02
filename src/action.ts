a7c11ce8f2aa10138450b48d0381b280890c0c0b
Miguel Casqueira <miguel@balena.io>
2021-09-02 01:33:48 -0400

    Implement storing Releases by environment context
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- src/action.ts
+++ src/action.ts
@@ -7,13 +7,6 @@ import * as balena from './balena-utils';
 import * as git from './git';
 
 export async function run(): Promise<void> {
-	// Name of the workflow this action is running in (to store and get data from)
-	const workflowName = process.env.GITHUB_WORKFLOW;
-	// If the workflow does not have a name then fail early
-	if (!workflowName) {
-		throw new Error('Workflow must contain a name');
-	}
-
 	// If the payload does not have a repository object then fail early
 	if (!context.payload.repository) {
 		throw new Error('Workflow payload was missing repository object');
@@ -21,8 +14,6 @@ export async function run(): Promise<void> {
 
 	// Get the master branch so we can infer intent
 	const target = context.payload.repository.master_branch;
-	// Environment to build releases in
-	const environment = core.getInput('environment', { required: false });
 	// Name of the fleet to build for
 	const fleet = core.getInput('fleet', { required: true });
 	// File path to release source code
@@ -38,18 +29,17 @@ export async function run(): Promise<void> {
 
 	// If a pull request was closed and merged then finalize releases!
 	if (
-		context.event.action === 'closed' &&
+		context.payload.action === 'closed' &&
 		context.payload.pull_request?.merged
 	) {
-		// Get all the previous releases built during this workflow filtered by environment
-		const previousReleases = await getReleases(workflowName, environment);
+		// Get all the previous releases built during this workflow
+		const previousReleases = await github.getReleases();
 		// Finalize all the releases
-		for (const r of previousReleases) {
-			await balena.finalize(r.id);
-		}
-		if (previousReleases.length > 0) {
-			// if there releases, clear them so subsequent calls to this action don't try to finalize already final releases
-			await clearReleases(workflowName, environment);
+		for (const rId in previousReleases) {
+			if (!previousReleases[rId].finalized) {
+				await balena.finalize(rId);
+				await github.saveRelease({ id: rId, finalized: true });
+			}
 		}
 		return; // Action is done!
 	}
@@ -58,74 +48,19 @@ export async function run(): Promise<void> {
 
 	// Indicate to action if repo uses Versionbot for versioning
 	const hasVersionbot = core.getInput('versionbot', { required: false });
-	// Extract info about the git repository
-	const { name: repoName } = github.repositoryMetaContext();
 
 	// If the repository uses Versionbot then checkout Versionbot branch
 	if (hasVersionbot) {
 		const versionbotBranch = await versionbot.getBranch();
 		// This will checkout the branch to the `GITHUB_WORKSPACE` path
-		await git.checkout(repoName, versionbotBranch);
+		await git.checkout(context.payload.repository.name, versionbotBranch);
 	}
 
 	// Now send the source to the builders which will build a draft
 	const releaseId = await balena.push(fleet, src);
 	// Persist built release to workflow
-	await setRelease(workflowName, environment, { id: releaseId });
+	await github.saveRelease({ id: releaseId, finalized: false });
 	// Set the built releaseId in the output
 	core.setOutput('releaseId', releaseId);
 	// Action is now done and will run again once we merge
-}
-
-/**
- * A release
- * @typedef {Object} Release
- * @property {string} id - ID provided from builders which identifies the release in balenaCloud
- */
-
-type Release = {
-	id: string;
-};
-
-/**
- * Allows you to persist a release to the workflouw output (do not get removed after 90 days like normal outputs)
- *
- * This function only accepts a single release because we only want to create 1 release per run.
- * However, it will find previously created releases and add to that list.
- *
- * @param {string} workflow - name of the workflow to set output for (typically the workflow running this action)
- * @param {string} environment - environment the release were built on
- * @param {release} data - release object to store
- *
- */
-
-async function setRelease(
-	workflowName: string,
-	environment: string,
-	data: Release,
-): Promise<void> {
-	await github.setOutput(workflowName, JSON.stringify({ releases: data }));
-}
-
-// This function returns an array of releases because when we finalize a release we will just do all
-/**
- * Fetches a list of releases attached to the output of the given workflow
- *
- * Since releases are stored by environment, the function will filter releases on that.
- *
- * @param {string} workflow - name of the workflow to get output from
- * @param {string} environment - environment the releases were built on
- *
- */
-
-async function getReleases(
-	workflowName: string,
-	environment: string,
-): Promise<Release[]> {
-	const output = await github.getOutput(workflowName);
-	const outputJSON = JSON.parse(output);
-	if (outputJSON.hasOwnProperty('releases')) {
-		return outputJSON.releases;
-	}
-	return [];
 }
--- src/github-utils.ts
+++ src/github-utils.ts
@@ -1,31 +1,129 @@
 import * as github from '@actions/github';
-import { exec } from '@actions/exec';
+import * as core from '@actions/core';
 
-type MetaContext = {
-	owner: string;
-	name: string;
-	ref: string;
+const token = core.getInput('github_token', { required: true });
+const environment = core.getInput('environment', { required: false });
+const octokit = github.getOctokit(token);
+const repoContext = {
+	owner: github.context.payload.repository?.owner.login || '',
+	name: github.context.payload.repository?.name || '',
+	ref: github.context.payload.pull_request?.base.sha || '',
 };
 
-export async function setOutput(
-	workflowName: string,
-	data: string,
-): Promise<void> {
-	return;
+/**
+ * Allows you to persist a release to the workflow output (does not get removed after 90 days like step/action outputs)
+ *
+ * @param {release} release - Release to persist
+ *
+ */
+
+export async function saveRelease(release: Release): Promise<void> {
+	// Get current output so we can add to it if needed
+	const output = await getOutput();
+	// If output doesn't have environment set then initiate object
+	if (!output.hasOwnProperty(environment)) {
+		output[environment] = {};
+	}
+	// Set the new Release in the ReleaseStore
+	output[environment][release.id] = release;
+	// Store new output
+	return setOutput(output);
 }
 
-export async function getOutput(workflowName: string): Promise<string> {
-	return 'abc';
+/**
+ * Get stored Releases from the workflow's output filtered by this action's environment context
+ *
+ * @return {Releases} Releases from the output
+ */
+
+export async function getReleases(): Promise<Releases> {
+	// Get workflow's output data
+	const output = await getOutput();
+	// Check if output contains a Releases for the environment
+	if (!output.hasOwnProperty(environment)) {
+		// No Releases found
+		return {};
+	}
+	// Return Release
+	return output[environment];
 }
 
-export function repositoryMetaContext(): MetaContext {
-	return {
-		owner: github.context.payload.repository.owner.login,
-		name: github.context.payload.repository.name,
-		ref: github.context.payload.pull_request.head.sha,
+async function getOutput(): Promise<ReleaseStore> {
+	const thisCheckRun = await getThisCheck();
+	if (!thisCheckRun.output.text) {
+		return {};
+	}
+	return JSON.parse(thisCheckRun.output.text);
+}
+
+async function setOutput(data: ReleaseStore): Promise<void> {
+	return updateRun(JSON.stringify(data));
+}
+
+async function getThisCheck(): Promise<CheckRun> {
+	// Make sure the context has a check run (workflow) name
+	if (!github.context.workflow) {
+		throw new Error('Workflow must contain a name');
+	}
+	// Get the checks for this commit
+	const checks = (
+		await octokit.request(
+			'GET /repos/{owner}/{repo}/commits/{ref}/check-runs',
+			{
+				owner: repoContext.owner,
+				repo: repoContext.name,
+				ref: repoContext.ref,
+			},
+		)
+	).data;
+	// Find the check run that matches the name in the context
+	const check = checks.check_runs.filter(
+		(c: CheckRun) => c.name === github.context.workflow,
+	)[0];
+	if (!check) {
+		throw new Error(
+			`Unable to find target ${github.context.workflow} in checks ran on commit ${repoContext.ref}.`,
+		);
+	}
+	return check;
+}
+
+async function updateRun(data: string) {
+	const thisCheckRun = await getThisCheck();
+	await octokit.request(
+		'PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}',
+		{
+			owner: repoContext.owner,
+			repo: repoContext.name,
+			check_run_id: thisCheckRun.id,
+			output: {
+				title: 'Build release',
+				summary: 'Succssfully built a release!',
+				text: data,
+			},
+		},
+	);
+}
+
+interface ReleaseStore {
+	[key: string]: Releases;
+}
+
+interface Releases {
+	[key: string]: Release;
+}
+
+type Release = {
+	id: string;
+	finalized: boolean;
+};
+
+type CheckRun = {
+	id: number;
+	name: string;
+	output: {
+		title: string;
+		summary: string;
+		text: string;
 	};
-}
-
-export function isPullRequest(): boolean {
-	return github.context.payload.hasOwnProperty('pull_request');
-}
+};
