cc2f074d049b493253be1ab45bc4aaf08c0a8712
Miguel Casqueira <miguel@balena.io>
2021-10-08 16:42:24 -0400

    Exit early if BALENA_TOKEN is empty
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ BALENARC_BALENA_URL=${BALENA_URL}
 
 export BALENARC_BALENA_URL
 
+[ -z "$BALENA_TOKEN" ] && echo "BALENA_TOKEN input cannot be empty" && exit 1
+
 balena login --token ${BALENA_TOKEN}
 
 balena whoami
