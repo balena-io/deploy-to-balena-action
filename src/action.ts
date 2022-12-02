0e4c019bb588fffc752a380d6dff1fe2e0b1e757
Alex Bucknall <alex.bucknall@gmail.com>
2022-12-02 18:19:02 +0000

    test command
    Change-type: patch
    Signed-off-by: Alex Bucknall <alex.bucknall@gmail.com>

--- src/action.ts
+++ src/action.ts
@@ -5,7 +5,7 @@ import * as core from '@actions/core';
 export async function test(device: string, command: string, timeout: number) {
     core.info(`Entering tests with ${command}`)
     const test = spawn(
-        "whoami",
+        "echo $PATH",
         {
             shell: true,
             stdio: 'inherit',
